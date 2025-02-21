e63a2e0f6785c7a5149ae94f093ab471cad0de3a
SMORALES241 <sandra.morales@est.iudigital.edu.co>
2025-02-21 18:22:45 +0000

    carga setup

---
+++ src/sandra_morales/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pad",
+    version="0.0.1",
+    author="Sandra Morales",
+    author_email="",
+    description="",
+    py_modules=["actividad_1"],
+    install_requires=[
+        "pandas",
+        "matplotlib"
+    ]
+    
+    
+)
